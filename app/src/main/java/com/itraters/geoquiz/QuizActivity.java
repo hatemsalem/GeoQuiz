78c21186f05756efaafba72537223bb22435bf49
Hatem Salem <hatem.salem@itraters.com>
2017-10-21 13:16:35 +0200

    Implement Chapter 01 Challenge

--- app/src/main/java/com/itraters/geoquiz/QuizActivity.java
+++ app/src/main/java/com/itraters/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.itraters.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,7 +11,7 @@ public class QuizActivity extends AppCompatActivity
 {
     private Button trueButton;
     private Button falseButton;
-
+    Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -18,12 +19,15 @@ public class QuizActivity extends AppCompatActivity
         setContentView(R.layout.activity_quiz);
         trueButton=(Button)findViewById(R.id.true_button);
         falseButton=(Button)findViewById(R.id.false_button);
+        toast=Toast.makeText(this,"",Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP,0,0);
         trueButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                toast.setText(R.string.correct_toast);
+                toast.show();
             }
         });
         falseButton.setOnClickListener(new View.OnClickListener()
@@ -31,7 +35,9 @@ public class QuizActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                toast.setText(R.string.incorrect_toast);
+                toast.show();
+
             }
         });
     }
